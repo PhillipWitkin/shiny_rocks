8ee97c4c62c529eec1f445be2415ff47a496ab8e
PhillipWitkin <pwitkin273@gmail.com>
2017-03-06 02:11:42 -0500

    another change, 2

--- src/HeyNow.java
+++ src/HeyNow.java
@@ -4,7 +4,7 @@ public class HeyNow {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("its a .... git gest");
-		int num = 5;
+		int num = 6; // and some stuff here
 		System.out.println();
 	}
 
