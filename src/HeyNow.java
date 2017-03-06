9ced84463bc7cf2755aaf8451e262eee8bffbb65
PhillipWitkin <pwitkin273@gmail.com>
2017-03-06 02:18:03 -0500

    a commit on newBranch1

--- src/HeyNow.java
+++ src/HeyNow.java
@@ -5,6 +5,7 @@ public class HeyNow {
 		// TODO Auto-generated method stub
 		System.out.println("its a .... git gest");
 		int num = 6; // and some stuff here
+		System.out.println(num);
 		System.out.println();
 	}
 
